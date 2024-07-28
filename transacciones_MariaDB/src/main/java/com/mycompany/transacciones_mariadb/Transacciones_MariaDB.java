974e771e6b883e887dd4e574560497256fe31a64
WilderL <menchuwilder@gmail.com>
2024-07-28 16:19:49 -0600

    Inializnaod prueba

---
+++ transacciones_MariaDB/src/main/java/com/mycompany/transacciones_mariadb/Transacciones_MariaDB.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.transacciones_mariadb;
+
+/**
+ *
+ * @author WilderL
+ */
+public class Transacciones_MariaDB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
